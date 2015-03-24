db29b8dbedb305e321a308f649d7a7c8f7afcbbe
bb-coder <bbcoderios@gmail.com>
2015-03-24 13:30:08 +0800

    Initial Commit

---
+++ BHBKit/BHBKit.h
@@ -0,0 +1,19 @@
+//
+//  BHBKit.h
+//  BHBKit
+//
+//  Created by bihongbo on 15/3/24.
+//  Copyright (c) 2015年 毕洪博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BHBKit.
+FOUNDATION_EXPORT double BHBKitVersionNumber;
+
+//! Project version string for BHBKit.
+FOUNDATION_EXPORT const unsigned char BHBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BHBKit/PublicHeader.h>
+
+
